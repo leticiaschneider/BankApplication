6729975f66d0e13ad0fcbb70f4677029971fe2fb
leticiaschneider <leticia.schneider@gmail.com>
2024-06-09 23:06:17 -0300

    finalize back account system

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,26 +1,40 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
+		List<Account> accounts = new LinkedList<Account>();
+		
 		String file = "/Users/leticiaschneider/eclipse-workspace/BankAccountApp/resources/newBankAccounts.csv";
-//		 Checking check1 = new Checking("Lety", "23233221", 1500);
-//		 Savings saving1 = new Savings("Rich Lowe", "3232222", 2500);
-//		 
-//		 saving1.showInfo();
-//		 System.out.println("*********");
-//		 check1.showInfo();
 		
 		List<String[]> newAccountHolders = utilities.CSV.read(file);
+		
 		for (String[] accountHolder : newAccountHolders) {
-			System.out.println("-------------");
-			System.out.println("New account");
-			System.out.println(accountHolder[0]);
-			System.out.println(accountHolder[1]);
-			System.out.println(accountHolder[2]);
-			System.out.println(accountHolder[3]);
+			String name = accountHolder[0];
+			String sSN = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+			
+//			System.out.println("O QUE APARECE" + accountHolder[2]);
+			
+			if (accountType.equals("Savings")) {
+				accounts.add(new Savings(name, sSN, initDeposit));
+			} 
+			else if (accountType.equals("Checking")) {
+				accounts.add(new Checking(name, sSN, initDeposit));
+			} 
+			else {
+				System.out.println("Error reading account type");
+			}
+		}
+		
+		
+		for (Account acc : accounts) {
+			System.out.println("------------------");
+			acc.showInfo();
 		}
 	}
 
