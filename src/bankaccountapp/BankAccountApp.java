024f7ec91d9b494e8e96c28d0b7948632a4db202
leticiaschneider <leticia.schneider@gmail.com>
2024-06-09 22:45:21 -0300

    writing a CSV Reader Utility

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,14 +1,27 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		 Checking check1 = new Checking("Lety", "23233221", 1500);
-		 Savings saving1 = new Savings("Rich Lowe", "3232222", 2500);
-		 
-		 saving1.showInfo();
-		 System.out.println("*********");
-		 check1.showInfo();
+		String file = "/Users/leticiaschneider/eclipse-workspace/BankAccountApp/resources/newBankAccounts.csv";
+//		 Checking check1 = new Checking("Lety", "23233221", 1500);
+//		 Savings saving1 = new Savings("Rich Lowe", "3232222", 2500);
+//		 
+//		 saving1.showInfo();
+//		 System.out.println("*********");
+//		 check1.showInfo();
+		
+		List<String[]> newAccountHolders = utilities.CSV.read(file);
+		for (String[] accountHolder : newAccountHolders) {
+			System.out.println("-------------");
+			System.out.println("New account");
+			System.out.println(accountHolder[0]);
+			System.out.println(accountHolder[1]);
+			System.out.println(accountHolder[2]);
+			System.out.println(accountHolder[3]);
+		}
 	}
 
 }
