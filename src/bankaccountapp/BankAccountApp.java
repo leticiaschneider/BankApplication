4bc8986a852aeee5b1f714f35dc99b93c2e53a12
leticiaschneider <leticia.schneider@gmail.com>
2024-06-09 21:23:32 -0300

    implement account setup, constructors, methods, interest rates, transactions, and access specifiers

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -3,8 +3,12 @@ package bankaccountapp;
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 Checking check1 = new Checking("Lety", "23233221", 1500);
+		 Savings saving1 = new Savings("Rich Lowe", "3232222", 2500);
+		 
+		 saving1.showInfo();
+		 System.out.println("*********");
+		 check1.showInfo();
 	}
 
 }
--- src/bankaccountapp/Savings.java
+++ src/bankaccountapp/Savings.java
@@ -1,5 +1,32 @@
 package bankaccountapp;
 
 public class Savings extends Account {
-
+	
+	private int safetyDepositBoxID;
+	private int safetyDepositBoxKey;
+	
+	public Savings(String name, String sSN, double initDeposit) {
+		super(name, sSN, initDeposit);
+		accountNumber = "1" + accountNumber;
+		setSafetyDepositBox();
+	}
+	
+	@Override
+	public void setRate() {
+		rate = getBaseRate() - .25;
+	}
+	
+	public void setSafetyDepositBox() {
+		safetyDepositBoxID = (int) (Math.random() * Math.pow(10, 3));
+		safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
+	}
+	
+	public void showInfo() {
+		super.showInfo();
+		System.out.println(
+				"Your Savings Account Features" +
+				"\n - Safety Deposit Box ID: " + safetyDepositBoxID +
+				"\n - Safety Deposit Box Key: " + safetyDepositBoxKey
+		);
+	}
 }
